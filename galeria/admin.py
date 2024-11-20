3c02ce1d87bcf1f469142f73e7ef94a661e38b10
Leonardo Veras <andradeveras96@gmail.com>
2024-11-20 14:24:19 -0300

    List tables in python admin

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from galeria.models import Fotografia
+
+class Listando_fotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda",)
+    list_display_links = ("id", "nome")
+    search_fields = ("nome",)
 
 
+admin.site.register(Fotografia, Listando_fotografias)
