266cec8b7ba9641e14e64c15e1ea2fa71f6d8f04
Leonardo Veras <andradeveras96@gmail.com>
2024-11-26 17:54:08 -0300

    Create a logic to login and error messages

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render, redirect
 from usuarios.forms import LoginForms, CadastroForms 
 from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
 
 def login(request):
     form = LoginForms()
+
+    if request.method == "POST":
+        form = LoginForms(request.POST) 
+
+        if form.is_valid():
+            nome = form["nome_login"].value()
+            senha = form["senha"].value()
+        usuario = auth.authenticate(
+            request,
+            username=nome,
+            password=senha
+        )
+        if usuario is not None:
+            auth.login(request, usuario)
+            messages.success(request, f"{nome} Logado com sucesso")
+            return redirect('index')
+        else:
+            messages.error(request, "Usuário ou senha inválidos")
+            return redirect('login')
+            
     return render(request, "usuarios/login.html", {"form": form})
 
 def cadastro(request):
@@ -14,12 +36,14 @@ def cadastro(request):
 
         if form.is_valid():
             if form["senha1"].value() != form["senha2"].value():
+                messages.error(request, "Senhas não conferem")
                 return redirect('cadastro')
             nome=form["nome_cadastro"].value()
             email=form["email"].value()
             senha=form["senha1"].value()
 
             if User.objects.filter(username=nome).exists():
+                messages.error(request, "Nome de usuário já cadastrado")
                 return redirect('cadastro')
 
             usuario = User.objects.create_user(
@@ -28,6 +52,7 @@ def cadastro(request):
                 password=senha
             )
             usuario.save()
+            messages.success(request, f"{nome} cadastrado com sucesso")
             return redirect('login')   
          
     return render(request, "usuarios/cadastro.html", {"form": form})
