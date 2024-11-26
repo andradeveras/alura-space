8e61f0a6ff3427becfdd6919e92607ef1755591f
Leonardo Veras <andradeveras96@gmail.com>
2024-11-26 11:50:56 -0300

    Rebuilding cadastro page form

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class LoginForms(forms.Form):
-    Name_login = forms.CharField(
+    name_login = forms.CharField(
         label = 'Nome de Login',
         required = True,
         max_length = 30,
@@ -20,6 +20,52 @@ class LoginForms(forms.Form):
             attrs={
                 "class": "form-control",
                 "placeholder": "Digite sua senha",
+            }
+        )
+    )
+
+class CadastroForms(forms.Form):
+    nome_cadastro = forms.CharField(
+        label = 'Nome completo',
+        required = True,
+        max_length = 100,
+        widget = forms.TextInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite seu nome completo"
+            }
+        )
+    )
+    email = forms.EmailField(
+        label = 'Email',
+        required = True,
+        max_length = 100,
+        widget = forms.EmailInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite seu email"
+            }
+        )
+    )
+    senha1 = forms.CharField(
+        label = 'Senha',
+        required = True,
+        max_length = 70,
+        widget = forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Digite sua senha"
+            }
+        )
+    )
+    senha2 = forms.CharField(
+        label = 'Confirmação de senha',
+        required = True,
+        max_length = 70,
+        widget = forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Confirme sua senha"
             }
         )
     )
